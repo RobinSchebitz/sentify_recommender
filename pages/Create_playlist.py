374da06239f806e4ba5657d50cd94e2c1fbfe78a
b-etsiwah <30800387+ehts2@users.noreply.github.com>
2024-05-17 18:08:24 +0200

    modified pages files

--- pages/Create_playlist.py
+++ pages/Create_playlist.py
@@ -10,38 +10,52 @@ def run():
     st.title(' ')
     st.title('Welcome ' + username)
     st.title(' ')
-    st.title('Choose a Playlist')
+    
     
     # Input field for songs
     #st.text_input("Enter Song Names", placeholder="Type song names here...")
 
+    col1, buff, col2 = st.columns([2,1,2])
+
+    with col1:
+        st.subheader('Choose a Playlist')
+        # Assuming playlists is a list of dictionaries where each dictionary has keys 'name' and 'id'
+        playlist_dict = {}
+        for playlist in playlists['items']:
+            playlist_dict[playlist['name']] = playlist['id']
+        
+        # Create a selectbox option for each playlist
+        selected_playlist_index = st.selectbox(username + "'s playlists", list(playlist_dict.keys()))
+
+        """ # Get the name of the selected playlist
+        selected_playlist_name = playlists[selected_playlist_index]['name']
+
+        # Display the selected playlist name
+        st.write(f"You selected: {selected_playlist_name}") """
+
+    with col2:
+        st.subheader('Or choose a Song')
+        user_input = st.text_input('Enter your favorite song:')   
+        
+        
+
+    col3, buff, col4 = st.columns([2,1,2])
     
-    # Assuming playlists is a list of dictionaries where each dictionary has keys 'name' and 'id'
-    playlists = [
-        {"name": "Playlist 1", "id": "id1"},
-        {"name": "Playlist 2", "id": "id2"},
-        {"name": "Playlist 3", "id": "id3"}
-    ]  # Replace this with your actual list of dictionaries
+    with col3:
+        if col3.button("Show Playlist"):
+                st.switch_page('pages/Preview.py')
 
-    # Create a radio button for each playlist
-    selected_playlist_index = st.radio(username + "'s playlists", range(len(playlists)), format_func=lambda i: playlists[i]['name'])
+    with col4:
+         if col4.button("Show Tracks"):
+            st.switch_page('pages/Preview.py')
+         
 
-    # Get the name of the selected playlist
-    selected_playlist_name = playlists[selected_playlist_index]['name']
-
-    # Display the selected playlist name
-    st.write(f"You selected: {selected_playlist_name}")
-
-
-    """# Button to navigate to the Settings page (Page 3)
+    
+    
+    # Button to navigate to the Settings page (Page 3)
     if st.button("Go to Preview"):
         st.switch_page('pages/Preview.py')
         
-    # Button to generate the playlist
-    if st.button("Generate Playlist"):
-        st.write("Playlist would be generated here.")
-        # Logic for generating the playlist would go here
-        st.switch_page('pages/')"""
                   
 
 if __name__ == "__main__":
